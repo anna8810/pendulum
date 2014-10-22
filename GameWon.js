4e4328603c91a248d7f2353f1856def8fc6cf8ed
annfl042 <annfl042@student.liu.se>
2014-10-22 07:17:59 +0200

    Added score when winning

--- GameWon.js
+++ GameWon.js
@@ -5,9 +5,13 @@ Pendulum.GameWon.prototype = {
 	create: function() {
 		// Background and text
 		startBG = this.add.image(0, 0, "startScreen");
-		winning = this.add.image(this.world.width/2, this.world.height/2-50, "winning");
+		winning = this.add.image(this.world.width/2, this.world.height/2-100, "winning");
 		winning.anchor.setTo(0.5, 0.5);
-		
+
+		yourScore = "Your score: " + nrOfPoints + " / " + pointsTotal;
+		score = this.add.text(this.world.width/2, this.world.height/2-10, yourScore, {fill: "#FFF", font:"40px Segoe"});
+		score.anchor.setTo(0.5, 0.5);
+
 		// Buttons
 		restart = this.add.button(this.world.width/2-200, this.world.centerY+120, "restart", this.restart, this);
 		restart.anchor.setTo(0.5, 0.5);
@@ -17,6 +21,8 @@ Pendulum.GameWon.prototype = {
 		}	
 		menu = this.add.button(this.world.width/2, this.world.centerY+250, "menu", this.menu, this);
 		menu.anchor.setTo(0.5, 0.5);
+
+		console.log(nrOfPoints);
 	},
 
 	restart: function() {
